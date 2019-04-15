ace47c524ad520cd82da1fc7dcf0babd40bcc41b
alwin099 <49295420+alwin099@users.noreply.github.com>
2019-04-15 12:04:44 +1000

    Update rot_ciph.c

--- rot_ciph.c
+++ rot_ciph.c
@@ -4,7 +4,7 @@
 *  Name: Alwin Wilson
 *  Student number: 3305199
 *  Date: 29/4/19
-*  Description: Encrypts and decrypts a message using the substitution cipher (Ceaser cipher) method.*/
+*  Description: Encrypts and decrypts a message using the substitution cipher (Caesar cipher) method.*/
 
 int main()
 {
@@ -23,6 +23,7 @@ printf("To exit the program enter 3.\n");
 scanf("%d", &option); //store option as an integer in the variable option.
 
 
+
 //Selection of options.
 switch(option)
 
@@ -35,50 +36,66 @@ switch(option)
         
         printf("Please enter the key\n"); //Prompts user to enter the key, which dictates the roation amount
         scanf("%d", &key); //Store the rotation amount as an integer in the variable key.
-        
+    
+        //Checks if the key is a valid value. If not, terminates program with error message.
+        if (key <= 0 || key >= 26) {
+		printf("Error, please make sure the key is between 0 and 26");
+		return 0;
+	    }
+
       for (i = 0; (i < 100 && message[i] != '\0'); i++)
-      {
+        {
           message[i] = message[i] + key; //encrypts the message by adding the particular key to the ASCII value.
           if (message[i] > 'z')
               message[i] = message[i] - 26;
-      }
+        }
       printf("The encrypted message is: %s\n", message); //Display encrypted message.
       break;
-      
+    } 
       
     //Executes if the user has entered 2, signifying decryption.
     case 2:
-    
+    {
         printf("Please enter the message to decrypt:\n"); //Prompts user to enter message to decrypt.
         scanf("%99s", message); // store message as a string in the variable message.
         
-        printf("Please enter the key\n"); //Prompts user to enter the key, which dictates the roation amount
+        printf("Please enter the key\n"); //Prompts user to enter the key, which dictates the rotation amount.
         scanf("%d", &key); //Store the rotation amount as an integer in the variable key.
+        
+        if (key <= 1 || key >= 26) 
+	    {   //Checks if n is a valid shift value. If not, ends program with error.
+		printf("Error, please make sure the key is between 0 and 26");
+		return 0;
+	    
         
         for (i = 0; (i < 100 && message[i] != '\0'); i++)
         {
-            message [i] = message[i] - key; //decrypts the message by subtracting the particluar key from thr ASCII value
+            message [i] = message[i] - key; //decrypts the message by subtracting the particluar key from the ASCII value
             if (message[i] > 'z')
                message[i] = message[i] - 26;
         }
         printf("The decrypted message is: %s\n", message); // Display encrypted message.
         break;
-        
+    }
         
         //Executes if the user has entered 3, signifying desire to exit the program.
         case 3:
+        {
         return(0);
         break;
         
         // Executes if none of the possible options are entered. 
         default:
         printf("Please choose a valid option\n"); //Displays if the user has not entered 1 or 2
+        }
     
 }  
  
+    }
+     return 0;
 }
-    return 0;
-}
+
+
 
 
 
