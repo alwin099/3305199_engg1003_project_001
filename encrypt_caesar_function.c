34f8f3f97af77289e0c5d9f8304c100f9a5d25ed
alwin099 <49295420+alwin099@users.noreply.github.com>
2019-04-25 16:00:01 +1000

    Update encrypt_caesar_function.c

--- encrypt_caesar_function.c
+++ encrypt_caesar_function.c
@@ -30,16 +30,26 @@ void encrypt_text(char text[], int key)
 
 int main()
 {
+  
+  //declare variables.        
   int key;
   char text[100];
 
-  printf("PLease enter message to be encrypted:\n");
-  scanf("%s", text);
+  printf("PLease enter message to be encrypted:\n"); //prompts user to type the message to be encrypted.
+  scanf("%[^\n]s", text); //stores input into the variable text, which is a string.
   
-  printf("Please enter the key:\n");
+  printf("Please enter the key:\n"); //prompts user to the enter the value by which the alphabet is rotated.
   scanf("%d", &key);
   
-  encrypt_text(text, key);
-  printf("The encrypted message is: %s\n", text);
+  //loop to check if the key that is entered is between 0 and 26.
+    if (key <= 0 || key >= 26) 
+    {
+		printf("Error, please make sure the key is between 0 and 26"); //displays an error message if the key is out of the range (0 - 26).
+		return 0; //exits the program due to error.
+    
+    }
+  
+  encrypt_text(text, key); //call the function to encrypt the message.
+  printf("The encrypted message is: %s\n", text); //display the encrypted message.
   return 0;
 }
