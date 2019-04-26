ea5f18475ce5c999385d49488b0f74cdca63fee9
alwin099 <49295420+alwin099@users.noreply.github.com>
2019-04-26 13:43:54 +1000

    Update encrypt_sub_function.c

--- encrypt_sub_function.c
+++ encrypt_sub_function.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 
+
+//function prototypes
 int locate_index(char cipher_alpha[], char alpha_locate);
-char* encrypt(char *text, char cipher_alpha[]);
+char* encrypt_sub(char *text, char cipher_alpha[]);
 
+
+//this function acquires the cipher index of each letter in the text.
 int locate_index(char cipher_alpha[], char alpha_locate)
     {
   for(int k = 0 ; k < 26; k ++){
-    if(cipher_alpha[k] == alpha_locate){
+    if(cipher_alpha[k] == alpha_locate) //compares between the letters in the text and the substitution alphabet.
+    {
       return k;
     }
   }
   return -1;
 } 
+ 
   
-  char* encrypt(char *text,char cipher_alpha[]) //Function prototype.
+  char* encrypt_sub(char *text,char cipher_alpha[]) //Function prototype.
     {
   int txtlength = strlen(text); //Acquire length of the text using the strlen function to know how many iterations is required.
   char *encrypted_text = (char *) malloc(sizeof(char)*txtlength);
@@ -37,24 +41,17 @@ int locate_index(char cipher_alpha[], char alpha_locate)
 
 int main()
 {
+ 
+  //declare variables
   char text[100];
   char cipher_alpha[26] = {'h','g','q','t','y','p','l','j','m','d','w','i','z','x','c','f','s','o','r','b','n','u','a','k','v','e'}; //substitution alphabet.
 
   
-  printf("Please enter the text to be encrypted\n");
-  scanf("%99[^\n]", text);
+  printf("Please enter the text to be encrypted\n"); //prompts user to type text to encrypt.
+  scanf("%99[^\n]", text); //stores input in the variable text, which is a string.
   
-  char *encrypted_text = encrypt(text, cipher_alpha);
-  printf("The encrypted text is: %s\n", encrypted_text); 
+  char *encrypted_text = encrypt_sub(text, cipher_alpha); //calls the function to decrypt the text.
+  printf("The encrypted text is: %s\n", encrypted_text); //displays the decrypted text.
   
   return 0;
 }
-
-
-
-  
-  
-  
-  
-  
-  
